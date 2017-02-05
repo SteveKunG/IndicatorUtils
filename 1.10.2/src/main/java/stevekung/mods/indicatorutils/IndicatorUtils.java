339f22cbea807c2461db28cb1c9bfb75008652ad
SteveKunG <mccommander_minecraft@hotmail.com>
2017-02-05 19:37:31 +0700

    1.10.2 Updates

--- 1.10.2/src/main/java/stevekung/mods/indicatorutils/IndicatorUtils.java
+++ 1.10.2/src/main/java/stevekung/mods/indicatorutils/IndicatorUtils.java
@@ -79,7 +79,7 @@ public class IndicatorUtils
     public static final String MOD_ID = "indicatorutils";
     public static final int MAJOR_VERSION = 2;
     public static final int MINOR_VERSION = 0;
-    public static final int BUILD_VERSION = 1;
+    public static final int BUILD_VERSION = 2;
     public static final String VERSION = IndicatorUtils.MAJOR_VERSION + "." + IndicatorUtils.MINOR_VERSION + "." + IndicatorUtils.BUILD_VERSION;
     public static final String MC_VERSION = (String) FMLInjectionData.data()[4];
     public static final String GUI_FACTORY = "stevekung.mods.indicatorutils.ConfigGuiFactory";
@@ -117,6 +117,7 @@ public class IndicatorUtils
         MinecraftForge.EVENT_BUS.register(new BlockhitAnimation());
         MinecraftForge.EVENT_BUS.register(new IndicatorUtilsEventHandler());
         MinecraftForge.EVENT_BUS.register(new NewChatEventHandler());
+        MinecraftForge.EVENT_BUS.register(new OldVersionWarningEventHandler());
         MinecraftForge.EVENT_BUS.register(this);
 
         if (event.getSide() == Side.CLIENT)
@@ -271,7 +272,7 @@ public class IndicatorUtils
                 }
                 if (!IndicatorUtils.STATUS_CHECK[0] && !IndicatorUtils.STATUS_CHECK[2] && VersionChecker.INSTANCE.isLatestVersion())
                 {
-                    event.player.addChatMessage(JsonMessageUtils.json("\"text\":\"New version of \",\"extra\":[{\"text\":\"Indicator Utils\",\"color\":\"aqua\",\"extra\":[{\"text\":\" is available \",\"color\":\"white\",\"extra\":[{\"text\":\"v" + VersionChecker.INSTANCE.getLatestVersion().replace("[" + IndicatorUtils.MC_VERSION + "]=", "") + " \",\"color\":\"green\",\"extra\":[{\"text\":\"for \",\"color\":\"white\",\"extra\":[{\"text\":\"MC-" + IndicatorUtils.MC_VERSION + "\",\"color\":\"gold\"}]}]}]}]}]"));
+                    event.player.addChatMessage(JsonMessageUtils.json("\"text\":\"New version of \",\"extra\":[{\"text\":\"Indicator Utils\",\"color\":\"aqua\",\"extra\":[{\"text\":\" is available \",\"color\":\"white\",\"extra\":[{\"text\":\"v" + VersionChecker.INSTANCE.getLatestVersionReplaceMC() + " \",\"color\":\"green\",\"extra\":[{\"text\":\"for \",\"color\":\"white\",\"extra\":[{\"text\":\"MC-" + IndicatorUtils.MC_VERSION + "\",\"color\":\"gold\"}]}]}]}]}]"));
                     event.player.addChatMessage(JsonMessageUtils.json("\"text\":\"Download Link \",\"color\":\"yellow\",\"extra\":[{\"text\":\"[CLICK HERE]\",\"color\":\"blue\",\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"" + TextFormatting.DARK_GREEN + "Click Here!\"},\"clickEvent\":{\"action\":\"open_url\",\"value\":\"" + URL + "\"}}]"));
                     IndicatorUtils.STATUS_CHECK[0] = true;
                 }
---
+++ 1.10.2/src/main/java/stevekung/mods/indicatorutils/OldVersionWarningEventHandler.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ *
+ * Copyright 2016 Wasinthorn Suksri/SteveKunG - Indicator Utils
+ *
+ ******************************************************************************/
+
+package stevekung.mods.indicatorutils;
+
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import stevekung.mods.indicatorutils.utils.VersionChecker;
+import stevekung.mods.indicatorutils.utils.gui.GuiOldVersionWarning;
+
+public class OldVersionWarningEventHandler
+{
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void onGuiOpen(GuiOpenEvent event)
+    {
+        if (VersionChecker.INSTANCE.isLatestVersion())
+        {
+            if (event.getGui() instanceof GuiMainMenu)
+            {
+                event.setGui(new GuiOldVersionWarning());
+            }
+        }
+        MinecraftForge.EVENT_BUS.unregister(this);
+    }
+}

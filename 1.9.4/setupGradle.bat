42a77461d55587a1e0609bf4d869fc2c85ca1c30
SteveKunG <mccommander_minecraft@hotmail.com>
2017-02-11 00:06:45 +0700

    Update workspace

--- 1.9.4/setupGradle.bat
+++ 1.9.4/setupGradle.bat
@@ -1,2 +1,2 @@
-set GRADLE_USER_HOME=M:\Minecraft Development\Modding Workspace\gradle_data\1.9.4
+set GRADLE_USER_HOME=M:\Minecraft Modding Workspace\.gradle_data\1.9.4
 gradlew setupDecompWorkspace eclipse

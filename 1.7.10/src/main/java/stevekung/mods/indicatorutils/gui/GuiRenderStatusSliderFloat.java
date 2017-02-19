3bf665e16cd5534548ca9c002f923607e53e477e
SteveKunG <mccommander_minecraft@hotmail.com>
2017-02-19 20:36:21 +0700

    1.7.10 Update

--- 1.7.10/src/main/java/stevekung/mods/indicatorutils/gui/GuiRenderStatusSliderFloat.java
+++ 1.7.10/src/main/java/stevekung/mods/indicatorutils/gui/GuiRenderStatusSliderFloat.java
@@ -16,19 +16,31 @@ import net.minecraft.util.MathHelper;
 import stevekung.mods.indicatorutils.config.ExtendedModSettings;
 
 @SideOnly(Side.CLIENT)
-public class GuiRenderStatusSlider extends GuiButton
+public class GuiRenderStatusSliderFloat extends GuiButton
 {
     private float sliderValue;
     public boolean dragging;
     private Options options;
 
-    public GuiRenderStatusSlider(int buttonId, int x, int y, Options option)
+    public GuiRenderStatusSliderFloat(int id, int x, int y, Options option)
     {
-        super(buttonId, x, y, 200, 20, "");
-        this.sliderValue = 1.0F;
+        super(id, x, y, 200, 20, "");
         this.options = option;
-        this.sliderValue = option.normalizeValue(this.getOptionValue(option));
-        this.displayString = option.getEnumString() + ": " + this.getOptionValue(option);
+        this.sliderValue = option.normalizeValue(ExtendedModSettings.RENDER_INFO_OPACITY);
+
+        if (this.sliderValue == 0.45000002F)
+        {
+            this.sliderValue = 0.45F;
+        }
+        if (this.sliderValue == 0.65000004F)
+        {
+            this.sliderValue = 0.65F;
+        }
+        if (this.sliderValue == 0.90000004F)
+        {
+            this.sliderValue = 0.9F;
+        }
+        this.displayString = option.getEnumString() + " " + this.sliderValue;
     }
 
     @Override
@@ -47,9 +59,22 @@ public class GuiRenderStatusSlider extends GuiButton
                 this.sliderValue = (float)(mouseX - (this.xPosition + 4)) / (float)(this.width - 8);
                 this.sliderValue = MathHelper.clamp_float(this.sliderValue, 0.0F, 1.0F);
                 float f = this.options.denormalizeValue(this.sliderValue);
-                this.setOptionValue(this.options, f);
                 this.sliderValue = this.options.normalizeValue(f);
-                this.displayString = this.options.getEnumString() + ": " + this.getOptionValue(this.options);
+
+                if (this.sliderValue == 0.45000002F)
+                {
+                    this.sliderValue = 0.45F;
+                }
+                if (this.sliderValue == 0.65000004F)
+                {
+                    this.sliderValue = 0.65F;
+                }
+                if (this.sliderValue == 0.90000004F)
+                {
+                    this.sliderValue = 0.9F;
+                }
+                ExtendedModSettings.RENDER_INFO_OPACITY = this.sliderValue;
+                this.displayString = this.options.getEnumString() + " " + this.sliderValue;
             }
             mc.getTextureManager().bindTexture(buttonTextures);
             GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
@@ -65,8 +90,8 @@ public class GuiRenderStatusSlider extends GuiButton
         {
             this.sliderValue = (float)(mouseX - (this.xPosition + 4)) / (float)(this.width - 8);
             this.sliderValue = MathHelper.clamp_float(this.sliderValue, 0.0F, 1.0F);
-            this.setOptionValue(this.options, this.options.denormalizeValue(this.sliderValue));
-            this.displayString = this.options.getEnumString() + ": " + this.getOptionValue(this.options);
+            ExtendedModSettings.RENDER_INFO_OPACITY = this.sliderValue;
+            this.displayString = this.options.getEnumString();
             this.dragging = true;
             return true;
         }
@@ -80,58 +105,12 @@ public class GuiRenderStatusSlider extends GuiButton
     public void mouseReleased(int mouseX, int mouseY)
     {
         this.dragging = false;
+        ExtendedModSettings.saveExtendedSettings();
     }
 
-    public void setOptionValue(Options settingsOption, float value)
-    {
-        int ivalue = (int) value;
-
-        if (settingsOption == Options.ARMOR_Y)
-        {
-            ExtendedModSettings.ARMOR_STATUS_OFFSET = ivalue;
-        }
-        else if (settingsOption == Options.POTION_Y)
-        {
-            ExtendedModSettings.POTION_STATUS_OFFSET = ivalue;
-        }
-        else if (settingsOption == Options.KEYSTOKE_X)
-        {
-            ExtendedModSettings.KEYSTROKE_X_OFFSET = ivalue;
-        }
-        else if (settingsOption == Options.KEYSTOKE_Y)
-        {
-            ExtendedModSettings.KEYSTROKE_Y_OFFSET = ivalue;
-        }
-    }
-
-    public int getOptionValue(Options settingOption)
-    {
-        if (settingOption == Options.ARMOR_Y)
-        {
-            return ExtendedModSettings.ARMOR_STATUS_OFFSET;
-        }
-        else if (settingOption == Options.POTION_Y)
-        {
-            return ExtendedModSettings.POTION_STATUS_OFFSET;
-        }
-        else if (settingOption == Options.KEYSTOKE_X)
-        {
-            return ExtendedModSettings.KEYSTROKE_X_OFFSET;
-        }
-        else if (settingOption == Options.KEYSTOKE_Y)
-        {
-            return ExtendedModSettings.KEYSTROKE_Y_OFFSET;
-        }
-        return 0;
-    }
-
-    @SideOnly(Side.CLIENT)
     public static enum Options
     {
-        ARMOR_Y("Armor Status Y Position", -512.0F, 512.0F, 1.0F),
-        POTION_Y("Potion Status Y Position", -512.0F, 512.0F, 1.0F),
-        KEYSTOKE_X("Keystroke (Advanced) X Position", -256.0F, 256.0F, 1.0F),
-        KEYSTOKE_Y("Keystroke Y Position", -512.0F, 512.0F, 1.0F);
+        RENDER_INFO_OPACITY("Render Info Opacity", 0.0F, 1.0F, 0.05F);
 
         private String enumString;
         private float valueStep;
@@ -151,32 +130,17 @@ public class GuiRenderStatusSlider extends GuiButton
             return this.enumString;
         }
 
-        public float getValueMin()
-        {
-            return this.valueMin;
-        }
-
-        public float getValueMax()
-        {
-            return this.valueMax;
-        }
-
-        public void setValueMax(float value)
-        {
-            this.valueMax = value;
-        }
-
         public float normalizeValue(float value)
         {
-            return MathHelper.clamp_float((this.snapToStepclamp_float(value) - this.valueMin) / (this.valueMax - this.valueMin), 0.0F, 1.0F);
+            return MathHelper.clamp_float((this.snapToStepClamp(value) - this.valueMin) / (this.valueMax - this.valueMin), 0.0F, 1.0F);
         }
 
         public float denormalizeValue(float value)
         {
-            return this.snapToStepclamp_float(this.valueMin + (this.valueMax - this.valueMin) * MathHelper.clamp_float(value, 0.0F, 1.0F));
+            return this.snapToStepClamp(this.valueMin + (this.valueMax - this.valueMin) * MathHelper.clamp_float(value, 0.0F, 1.0F));
         }
 
-        public float snapToStepclamp_float(float value)
+        public float snapToStepClamp(float value)
         {
             value = this.snapToStep(value);
             return MathHelper.clamp_float(value, this.valueMin, this.valueMax);

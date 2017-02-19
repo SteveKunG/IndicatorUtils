fafe93ce7c32efd8468c394fd7559019ced7c074
SteveKunG <mccommander_minecraft@hotmail.com>
2017-02-19 20:55:01 +0700

    Added New AFK Mode (360 and 360move)

--- 1.11.2/src/main/java/stevekung/mods/indicatorutils/handler/IndicatorUtilsEventHandler.java
+++ 1.11.2/src/main/java/stevekung/mods/indicatorutils/handler/IndicatorUtilsEventHandler.java
@@ -720,6 +720,22 @@ public class IndicatorUtilsEventHandler
                     this.mc.player.turn(angle, angle);
                 }
             }
+            else if (IndicatorUtilsEventHandler.AFK_MODE.equalsIgnoreCase("360"))
+            {
+                if (this.mc.player != null)
+                {
+                    this.mc.player.turn(5.0F, 0.0F);
+                }
+            }
+            else if (IndicatorUtilsEventHandler.AFK_MODE.equalsIgnoreCase("360move"))
+            {
+                if (this.mc.player != null)
+                {
+                    this.mc.player.turn(5.0F, 0.0F);
+                }
+                ++IndicatorUtilsEventHandler.AFK_MOVE_TICK;
+                IndicatorUtilsEventHandler.AFK_MOVE_TICK %= 8;
+            }
             else
             {
                 if (this.mc.player != null)
